4562f79edeaee35d1940f4d0594cd98df1371e5d
darkwolf <99601878+darkwolfboy09@users.noreply.github.com>
2022-08-03 19:08:15 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function setup(){
+    canvas = createCanvas(400,400);
+    canvas.position(550,200);
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.position(550,200)
+  }
+  function draw(){
+  
+  }
+  function preload(){
+  
+  }
